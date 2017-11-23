e3a9d42e72d9b5676d93785e3d9e1bd79c27dc8f
Gabriel West <gabe_west1@yahoo.com>
2017-11-23 02:34:07 -0600

    Navbar extends Responsive.

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Responsive } from "../Responsive" 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  constructor() { }
+export class NavbarComponent extends Responsive implements OnInit {
+  constructor() { super() }
 
   ngOnInit() {
+    console.log("isMobile:", this.isMobile)
   }
 
 }
