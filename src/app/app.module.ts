3251e727259e1b6295dc56ea84a01111a2741826
Gabriel West <gabe_west1@yahoo.com>
2017-11-22 18:16:46 -0600

    Included BreakpointsService in the providers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 
+import { BreakpointsService } from "./services/breakpoints.service"
 
 @NgModule({
   declarations: [
@@ -14,7 +14,9 @@ import { HeaderComponent } from './components/shared/header/header.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    BreakpointsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
