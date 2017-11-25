a4fd21dd74dfc30c7f5149f41277ed32ac0fc9fd
Gabriel West <gabe_west1@yahoo.com>
2017-11-24 19:02:19 -0600

    Setup the markup and styles for the navbar to be a pusher.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Responsive } from "./components/shared/Responsive" 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends Responsive {
   title = 'app';
 }
