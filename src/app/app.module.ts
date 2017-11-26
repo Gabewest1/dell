43e3823984b144b10d84737551ed52aebc321456
Gabriel West <gabe_west1@yahoo.com>
2017-11-25 23:47:08 -0600

    Moved the carousel into the components/shared directory.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeaderComponent } from './components/shared/header/header.component'
 import { NavbarComponent } from './components/shared/navbar/navbar.component'
 
 import { SideBarService } from "./services/side-bar.service";
-import { CarouselComponent } from './components/carousel/carousel.component'
+import { CarouselComponent } from './components/shared/carousel/carousel.component'
 import { FooterComponent } from './components/shared/footer/footer.component'
 
 @NgModule({
