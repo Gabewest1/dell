64d5998f4cc7dc9eafc476c8c3f803cd25cccf63
Gabriel West <gabe_west1@yahoo.com>
2017-11-28 20:29:00 -0600

    Updated navbar directive:
    Added function getIconStyles() to return the proper icon.

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -8,11 +8,41 @@ import { Responsive } from "../Responsive"
 })
 export class NavbarComponent extends Responsive implements OnInit {
   activeListItem: number
+  activeSublistItem: number
 
   constructor() { super() }
 
   ngOnInit() {
     this.activeListItem = -1
+    this.activeSublistItem = -1
+  }
+
+  getIconStyles(list, sublist) {
+    let styles = { fa: true }
+
+    if (!sublist) {
+      getListItemIcon.call(this)
+    } else {
+      getSublistItemIcon.call(this)
+    }
+
+    function getListItemIcon() {
+      if (list === this.activeListItem) {
+        styles["fa-caret-up"] = true
+      } else {
+        styles["fa-caret-down"] = true
+      }
+    }
+
+    function getSublistItemIcon() {
+      if (list === this.activeListItem && sublist === this.activeSublistItem) {
+        styles["fa-caret-up"] = true
+      } else {
+        styles["fa-caret-down"] = true
+      }
+    }
+
+    return styles
   }
 
   setActiveListItem(listItem) {
@@ -22,6 +52,18 @@ export class NavbarComponent extends Responsive implements OnInit {
       this.activeListItem = -1
     } else {
       this.activeListItem = listItem
+    }
+  }
+
+  setActiveSublistItem(event, listItem) {
+    event.stopPropagation()
+
+    let clickedAlreadyActiveListItem = listItem === this.activeSublistItem
+    
+    if (clickedAlreadyActiveListItem) {
+      this.activeSublistItem = -1
+    } else {
+      this.activeSublistItem = listItem
     }
   }
 }
