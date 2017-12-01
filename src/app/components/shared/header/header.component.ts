3e4ef4ddafcb825dd83b05f9b67a937030ae1e35
Gabriel West <gabe_west1@yahoo.com>
2017-11-30 18:07:19 -0600

    Updated home component:
    Made the search bar for the mobile display and hide when the search icon is clicked.

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -8,14 +8,20 @@ import { SideBarService } from "../../../services/side-bar.service"
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent extends Responsive implements OnInit {
+  isMobileSearchBarActive: boolean
 
   constructor(private sideBar: SideBarService) { super() }
 
   ngOnInit() {
+    this.isMobileSearchBarActive = false
   }
 
   toggleSideBar() {
     console.log("TOGGLING THE SIDEBAR")
     this.sideBar.toggleSideBar()
+  }
+
+  toggleSearchBar() {
+    this.isMobileSearchBarActive = !this.isMobileSearchBarActive
   }
 }
